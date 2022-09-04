e77f433be02ad8d8d00b51f58693cebb322611f8
Huu Chi Huynh <73843190+HuuChiHuynh@users.noreply.github.com>
2022-09-04 20:04:29 +0100

    Forgot to initalize the image renderer

--- src/StreamerChannel.ts
+++ src/StreamerChannel.ts
@@ -3,39 +3,40 @@ import { ImageRenderer } from './ChatRenderer/ImageRenderer';
 
 
 export class StreamerChannel {
-    name: string;
-    streamerId: string;
+	name: string;
+	streamerId: string;
 
-    previousStream: HelixStream | null = null;
-    stream: HelixStream | null = null;
+	previousStream: HelixStream | null = null;
+	stream: HelixStream | null = null;
 
-    imageRenderer: ImageRenderer;
+	imageRenderer: ImageRenderer;
 
-    constructor(streamerId: string, name: string) {
-        this.streamerId = streamerId;
-        this.name = name;
-        this.imageRenderer = new ImageRenderer(streamerId);
-    }
-    public async checkLiveStream(apiClient: ApiClient): Promise<StreamStatus> {
-        this.previousStream = this.stream;
-        this.stream = await apiClient.streams.getStreamByUserName(this.name);
-        if (this.stream != null) {
-            if (this.previousStream == null) {
-                return StreamStatus.NOW_LIVE;
-            }
-            return StreamStatus.STILL_LIVE;
-        } else {
-            if (this.previousStream != null) {
-                return StreamStatus.NOW_OFFLINE;
-            }
-            return StreamStatus.STILL_OFFLINE;
-        }
-    }
+	constructor(streamerId: string, name: string) {
+		this.streamerId = streamerId;
+		this.name = name;
+		this.imageRenderer = new ImageRenderer(streamerId);
+		this.imageRenderer.initalise();
+	}
+	public async checkLiveStream(apiClient: ApiClient): Promise<StreamStatus> {
+		this.previousStream = this.stream;
+		this.stream = await apiClient.streams.getStreamByUserName(this.name);
+		if (this.stream != null) {
+			if (this.previousStream == null) {
+				return StreamStatus.NOW_LIVE;
+			}
+			return StreamStatus.STILL_LIVE;
+		} else {
+			if (this.previousStream != null) {
+				return StreamStatus.NOW_OFFLINE;
+			}
+			return StreamStatus.STILL_OFFLINE;
+		}
+	}
 }
 
 export enum StreamStatus {
-    NOW_LIVE,
-    NOW_OFFLINE,
-    STILL_LIVE,
-    STILL_OFFLINE
+	NOW_LIVE,
+	NOW_OFFLINE,
+	STILL_LIVE,
+	STILL_OFFLINE
 }
